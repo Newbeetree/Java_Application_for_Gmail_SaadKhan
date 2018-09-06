b6ff8d108610eed0d9cda2b4003ef38fa8fbaf0a
khandragon <saaadkhan23@yahoo.ca>
2018-09-06 17:04:56 -0400

    copy main send from demo and rearange files

---
+++ src/test/SendEmailTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SendEmailTest {
+    private final String emailSend = "send.1633839@gmail.com";
+    private final String emailReceive = "receive.1633839@gmail.com";
+    private final String emailSendPwd = "sendpassword";
+    private final String emailReceivePwd = "receivepassword";
+    private final String emailCC1 = "other.1633839@gmail.com";
+
+    @Test
+    public void send(String user, String password, String receiver){
+
+    }
+}
