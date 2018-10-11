d916d623e94d84d92c4e201116798ff530594049
khandragon <saaadkhan23@yahoo.ca>
2018-10-11 14:09:49 -0400

    create proper files and folders

---
+++ src/main/java/com/saadkhan/controller/mainController.java
@@ -0,0 +1,16 @@
+package com.saadkhan.controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class mainController {
+
+}
